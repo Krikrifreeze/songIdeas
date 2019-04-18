b2423a8e38083459efd288871d06f4924913d906
Krikrifreeze <krikrifreeze@gmail.com>
2019-04-18 15:08:59 -0700

    small code tweaks

--- Main.py
+++ Main.py
@@ -3,15 +3,15 @@ import Chord, random
 def generate(n): #generates n chords in scale
     chords = []
     octave = 5
-    for i in range(n):
+    for _i in range(n):
         randomChordNum = random.randint(1,7) #generates chord number
         randomChordType = random.randint(0,1) #TODO: incorporate chord qualities
         chords.append(Chord.Chord(randomChordNum,randomChordType,octave))
     return chords
 
-def getKey(n):
+def getKey(n): #returns a string from the keys file based on n. (ex. n = 0 returns C, n = 11 returns B)
     keys = open("keys", "r")
-    for i in range(n):
+    for _i in range(n):
         key = keys.readline()
         #print(key)
     key = keys.readline()
